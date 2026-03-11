4dfc416159d5ef137c4b7322a48909c69ce74d6e
yalamuri-babu <yalj.gana@gmail.com>
2026-03-11 08:43:33 +0530

    Initial Spring Boot platform demo

---
+++ src/main/java/com/devops/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.devops.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from DevOps Platform Engineering!";
+    }
+}
---
+++ src/test/java/com/devops/platform_demo/PlatformDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.platform_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
